01b52f91e507cd1e5f12f4f0b1085b3cc1d600ac
Nikita Novitskiy <nvnovitskiy@icloud.com>
2022-11-04 22:34:29 +0400

    update main.py

--- main.py
+++ main.py
@@ -1,43 +1,49 @@
 import os
+import pickle
 import sys
+from datetime import datetime
+
 import numpy as np
 import pandas as pd
-import pickle
+from loguru import logger
 
 
 def classify(model: str, data: str):
+    """function to run predictions."""
     if not os.path.exists(model):
-        raise FileNotFoundError('Файл с моделью отсутствует.')
-    
+        raise FileNotFoundError('No file with the model.')
+
     if not os.path.exists(data):
-        raise FileNotFoundError('Файл с данными отсутствует.')
-        
-        
+        raise FileNotFoundError('No data file is available.')
+
+    logger.debug(f'Reading data from a file: {data}.')
     df = pd.read_csv(data)
-    print('Файл с данными считан успешно. Перехожу к загрузке модели.')
-    
+    logger.debug(
+        'The data file was read successfully. Moving on to loading the model.')
+
     model = pickle.load(open(model, 'rb'))
-    print('Модель загружена. Перехожу к предсказаниям.')
-    
+    logger.debug(f'The model: {model} is loaded. Moving on to predictions.')
+
     predict = np.array(model.predict(df))
-    print('Предсказания готовы.')
-    
+    logger.debug('The predictions are ready.')
+
     return predict
 
 
 if __name__ == '__main__':
-    model = input('Введите путь до файла с моделью: ')
-    data = input('Введите путь до файла с данными: ')
-    
+    model = input('Enter the path to the file with the model: ')
+    data = input('Enter the path to the data file: ')
+
     predict = classify(model, data)
-    print('Хотите сохранить результат в Excel?\n 1 - Нет\n 2 - Да')
-    choice = int(input('Сохраняем?: '))
-    
+    print('Do you want to save the result to Excel?\n 1 - No\n 2 - Yes')
+    choice = int(input('Saving?: '))
+
     if choice == 1:
-        print('Вывожу predict в консоль: ')
-        print('is_bot')
+        print('Output predict in the console: ')
         for pred in predict:
-            print(pred)
-    elif choice == 2:
-        pd.DataFrame(data=predict, columns=['is_bot']).to_csv('result.csv', index=False)
-        print(f'Предсказания сохранены в файл result.csv')
+            print(f'is_bot: {pred}')
+    else:
+        now = datetime.now().strftime('%d-%m-%Y-%H-%M-%S')
+        pd.DataFrame(data=predict, columns=['is_bot']).to_csv(
+            f'results/predictions_{now}.csv', index=False)
+        print(f'the predictions were saved to the folder "results".')
